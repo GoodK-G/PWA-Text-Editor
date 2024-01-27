beb8a2a3674371f6ee7aaf19ff313190f04fbbd1
GoodK-G <141077856+GoodK-G@users.noreply.github.com>
2024-01-27 23:50:33 +0000

    added some of the plugins for the CSS loader and the Service Worker.

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -2,6 +2,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
+const CssWebpackPlugin = require('mini-css-extract-plugin');
+const Babel = require('babel-loader');
+const Workbox = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
@@ -17,7 +21,21 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    //Adding the plugins for the manifest and service worker
+    //Adding the CSS loader
     plugins: [
+      CssWebpackPlugin.loader(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        filename: 'index.html',
+        chunks: ['main']
+      }),
+      //Generating the Service Worker
+      GenerateSW({
+        swDest: 'sw.js',      
+      }),
+      //Generating the Manifest,
+      
       
     ],
 
